73c0141bee524dee4389e8b8fa402e68a63168d9
YuvalGur135 <75333129+YuvalGur135@users.noreply.github.com>
2021-01-12 22:53:38 +0200

    started a test class

---
+++ src/main/java/frc/robot/vision/PoopTest.java
@@ -0,0 +1,9 @@
+package frc.robot.vision;
+
+import vision.limelight.Limelight;
+
+public class PoopTest {
+    public static void main(String[] args) {
+        // Limelight ploop = new Limelight();
+    }
+}
