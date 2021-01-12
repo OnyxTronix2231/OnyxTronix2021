0bfdc2d4da7ccfd2475093c63502c39f25d417d0
The Clu(e) Of The Pen <31019049+CluePen@users.noreply.github.com>
2021-01-12 23:34:46 +0200

    Added basic files for skills competition

---
+++ src/main/java/frc/robot/skillscompetiotion/SkillsConstants.java
@@ -0,0 +1,72 @@
+package frc.robot.skillscompetiotion;
+
+import frc.robot.drivetrain.Pose;
+
+import java.util.List;
+
+public class SkillsConstants {
+  public static class Waypoints {
+    public static final Waypoint A1 = new Waypoint(30, 150);
+    public static final Waypoint A3 = new Waypoint(90, 150);
+    public static final Waypoint A4 = new Waypoint(120, 150);
+    public static final Waypoint A6 = new Waypoint(180, 150);
+    public static final Waypoint A7 = new Waypoint(210, 150);
+    public static final Waypoint A8 = new Waypoint(240, 150);
+    public static final Waypoint A9 = new Waypoint(270, 150);
+    public static final Waypoint A10 = new Waypoint(300, 150);
+
+    public static final Waypoint B1 = new Waypoint(30, 120);
+    public static final Waypoint B2 = new Waypoint(60, 120);
+    public static final Waypoint B3 = new Waypoint(90, 120);
+    public static final Waypoint B4 = new Waypoint(120, 120);
+    public static final Waypoint B5 = new Waypoint(150, 120);
+    public static final Waypoint B6 = new Waypoint(180, 120);
+    public static final Waypoint B7 = new Waypoint(210, 120);
+    public static final Waypoint B8 = new Waypoint(240, 120);
+    public static final Waypoint B9 = new Waypoint(270, 120);
+    public static final Waypoint B10 = new Waypoint(300, 120);
+    public static final Waypoint B11 = new Waypoint(330, 120);
+
+    public static final Waypoint C3 = new Waypoint(90, 90);
+    public static final Waypoint C9 = new Waypoint(270, 90);
+
+    public static final Waypoint D1 = new Waypoint(30, 60);
+    public static final Waypoint D2 = new Waypoint(60, 60);
+    public static final Waypoint D3 = new Waypoint(90, 60);
+    public static final Waypoint D4 = new Waypoint(120, 60);
+    public static final Waypoint D5 = new Waypoint(150, 60);
+    public static final Waypoint D6 = new Waypoint(180, 60);
+    public static final Waypoint D7 = new Waypoint(210, 60);
+    public static final Waypoint D8 = new Waypoint(240, 60);
+    public static final Waypoint D9 = new Waypoint(270, 60);
+    public static final Waypoint D10 = new Waypoint(300, 60);
+    public static final Waypoint D11 = new Waypoint(330, 60);
+
+    public static final Waypoint E1 = new Waypoint(30, 30);
+    public static final Waypoint E3 = new Waypoint(90, 30);
+    public static final Waypoint E4 = new Waypoint(120, 30);
+    public static final Waypoint E6 = new Waypoint(180, 30);
+    public static final Waypoint E7 = new Waypoint(210, 30);
+    public static final Waypoint E8 = new Waypoint(240, 30);
+    public static final Waypoint E9 = new Waypoint(270, 30);
+    public static final Waypoint E10 = new Waypoint(300, 30);
+  }
+
+  public static class Paths {
+    public static final List<Pose> RED_FIRST_PATH = List.of(
+
+    );
+
+    public static final List<Pose> RED_SECOND_PATH = List.of(
+
+    );
+
+    public static final List<Pose> BLUE_FIRST_PATH = List.of(
+
+    );
+
+    public static final List<Pose> BLUE_SECOND_PATH = List.of(
+
+    );
+  }
+}
---
+++ src/main/java/frc/robot/skillscompetiotion/Waypoint.java
@@ -0,0 +1,26 @@
+package frc.robot.skillscompetiotion;
+
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+
+public class Waypoint {
+  private final double x;
+  private final double y;
+
+  public Waypoint(double x, double y) {
+    this.x = x * 0.0254;
+    this.y = y * 0.0254;
+  }
+
+  public Pose2d getPose2dFromRotation(Rotation2d rotation){
+    return new Pose2d(x, y, rotation);
+  }
+
+  public double getX() {
+    return x;
+  }
+
+  public double getY() {
+    return y;
+  }
+}
