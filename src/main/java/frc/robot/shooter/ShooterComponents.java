fdc8568a0af71c9e6bcac1698cd9ff8d86a1a7f2
YuvalRashman <56125252+YuvalRashman@users.noreply.github.com>
2021-01-16 12:45:55 +0200

    delete imports

--- src/main/java/frc/robot/shooter/ShooterComponents.java
+++ src/main/java/frc/robot/shooter/ShooterComponents.java
@@ -1,10 +1,8 @@
 package frc.robot.shooter;
 
 import com.ctre.phoenix.motorcontrol.IMotorController;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.simulation.FlywheelSim;
 import pid.CtreMotionMagicController;
 import pid.CtrePIDController;
