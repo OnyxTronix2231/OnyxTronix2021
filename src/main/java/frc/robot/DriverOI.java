c05b01eeeb6df11b97dcb5a03d918d82a4f54300
YuvalRashman <56125252+YuvalRashman@users.noreply.github.com>
2021-01-20 19:26:46 +0200

    reverted Robot.java and DriverOi.java

--- src/main/java/frc/robot/DriverOI.java
+++ src/main/java/frc/robot/DriverOI.java
@@ -1,27 +1,16 @@
 package frc.robot;
 
+import static frc.robot.RobotConstants.DRIVER_JOYSTICK_PORT;
+
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.drivetrain.DriveTrain;
 import frc.robot.drivetrain.DriveTrainOiBinder;
-import frc.robot.shooter.DriverShooterOiBinder;
-import frc.robot.shooter.Shooter;
-import onyxTronix.JoystickAxis;
-
-import static frc.robot.RobotConstants.DRIVER_JOYSTICK_PORT;
 
 public class DriverOI {
 
-    public DriverOI(DriveTrain driveTrain, Shooter shooter) {
+    public DriverOI(DriveTrain driveTrain) {
         XboxController xboxController = new XboxController(DRIVER_JOYSTICK_PORT);
 
         new DriveTrainOiBinder(driveTrain, xboxController);
-
-        Trigger kXChangeAngle = new JoystickButton(xboxController, XboxController.Button.kX.value);
-
-        JoystickAxis shootByPresentOutPout = new JoystickAxis(xboxController, XboxController.Axis.kRightX.value);
-
-        new DriverShooterOiBinder(shooter, kXChangeAngle, shootByPresentOutPout);
     }
 }
