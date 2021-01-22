f0a8490c33d627f6009f17330f1676cdb6df1283
NadaVibe <nadav8775@gmail.com>
2021-01-22 18:48:55 +0200

    reset DriverOI and Robot so there wouldn't be conflicts

--- src/main/java/frc/robot/DriverOI.java
+++ src/main/java/frc/robot/DriverOI.java
@@ -3,26 +3,14 @@ package frc.robot;
 import static frc.robot.RobotConstants.DRIVER_JOYSTICK_PORT;
 
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.ballTrigger.BallTrigger;
-import frc.robot.collector.Collector;
-import frc.robot.commandGroups.DriverConveyorOIBinder;
 import frc.robot.drivetrain.DriveTrain;
 import frc.robot.drivetrain.DriveTrainOiBinder;
-import frc.robot.revolver.Revolver;
 
 public class DriverOI {
 
-  public DriverOI(DriveTrain driveTrain, BallTrigger ballTrigger, Collector collector, Revolver revolver) {
+  public DriverOI(DriveTrain driveTrain) {
     XboxController xboxController = new XboxController(DRIVER_JOYSTICK_PORT);
 
     new DriveTrainOiBinder(driveTrain, xboxController);
-
-    Trigger kBCollectAndLoad = new JoystickButton(xboxController, XboxController.Button.kB.value);
-    Trigger kASpinRevolverAndTrigger = new JoystickButton(xboxController, XboxController.Button.kA.value);
-    Trigger kXSpinRevolverAndTriggerThenOpenPiston = new JoystickButton(xboxController, XboxController.Button.kX.value);
-    new DriverConveyorOIBinder(collector, ballTrigger, revolver, kBCollectAndLoad, kASpinRevolverAndTrigger,
-            kXSpinRevolverAndTriggerThenOpenPiston);
   }
 }
