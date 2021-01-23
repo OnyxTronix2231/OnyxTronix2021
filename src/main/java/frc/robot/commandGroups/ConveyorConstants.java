6aee8c90291ba490b3772e48657848a05767d82f
NadaVibe <nadav8775@gmail.com>
2021-01-23 13:51:42 +0200

    Reformatted code by CR

---
+++ src/main/java/frc/robot/commandGroups/ConveyorConstants.java
@@ -0,0 +1,9 @@
+package frc.robot.commandGroups;
+
+public class ConveyorConstants {
+
+    public class ConveyorConstantsA {
+        static final double REVOLVER_RPM_WHILE_COLLECTING = 1394.53125;
+        static final double REVOLVER_RPM_WHILE_SHOOTING = 3187.5;
+    }
+}
