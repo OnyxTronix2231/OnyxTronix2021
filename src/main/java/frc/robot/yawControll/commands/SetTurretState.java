e1d6d9a48dd039c83af75edb78e656447715b393
OfekNeuberger <75333158+OfekNeuberger@users.noreply.github.com>
2021-01-26 22:44:27 +0200

    added set turret state

---
+++ src/main/java/frc/robot/yawControll/commands/SetTurretState.java
@@ -0,0 +1,13 @@
+package frc.robot.yawControll.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.yawControll.YawControl;
+import frc.robot.yawControll.YawControl.TurretState;
+
+public class SetTurretState extends InstantCommand {
+
+    public SetTurretState(YawControl yawControl, TurretState turretState) {
+        super(()-> yawControl.setTurretState(turretState));
+    }
+}
