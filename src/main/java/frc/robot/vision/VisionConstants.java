a96225d05c59155231d0bb57d631feedfb88d95f
YuvalGur135 <75333129+YuvalGur135@users.noreply.github.com>
2021-01-30 20:45:01 +0200

    updated constants

--- src/main/java/frc/robot/vision/VisionConstants.java
+++ src/main/java/frc/robot/vision/VisionConstants.java
@@ -11,13 +11,13 @@ public class VisionConstants {
     static final double BLUE_AREA_PERCENTAGE = 0;   //TODO:check and change
     static final double AREA_TOLARANCE = 0;    //TODO:check and change
 
-    static final double LIMELIGHT_ANGLE_TO_HORIZON = 0; //TODO: check and change // mechanical parameter
-    static final double LIMELIGHT_HEIGHT_TO_FLOOR = 0; //TODO: check and change // mechanical parameter
+    static final double LIMELIGHT_ANGLE_TO_HORIZON = 34.08; //TODO: check and change // mechanical parameter
+    static final double LIMELIGHT_HEIGHT_TO_FLOOR = 632.82; //TODO: check and change // mechanical parameter
 
     static final double TARGET_HEIGHT = 0; //TODO: check and change
     static final double DEFAULT_TARGET_DISTANCE = 0; //TODO: check and change
 
-    public static final double LIMELIGHT_TURRET_CENTER_CM =0; //TODO: check and change // mechanical parameter
+    public static final double LIMELIGHT_TURRET_CENTER_CM =18.79; //TODO: check and change // mechanical parameter
     public static final Vector2dEx VECTOR_LIMELIGHT_TURRET_CENTER = new Vector2dEx(LIMELIGHT_TURRET_CENTER_CM, 0);
 
     public static final double HEIGHT_OFFSET_INNER_OUTER_CENTER = 0; //TODO: check and change // mechanical parameter
