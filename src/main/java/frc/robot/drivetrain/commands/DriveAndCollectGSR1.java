4b2089f5bdf2a586776fdf34c635054a2e44baac
AE173 <56049042+AE173@users.noreply.github.com>
2021-03-10 17:58:20 +0200

    Corrected paths in commands

--- src/main/java/frc/robot/drivetrain/commands/DriveAndCollectGSR1.java
+++ src/main/java/frc/robot/drivetrain/commands/DriveAndCollectGSR1.java
@@ -1,6 +1,7 @@
 package frc.robot.drivetrain.commands;
 
 import static frc.robot.drivetrain.skills.SkillsConstants.Paths.GALACTIC_SEARCH_BLUE_FIRST;
+import static frc.robot.drivetrain.skills.SkillsConstants.Paths.GALACTIC_SEARCH_RED_FIRST;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
@@ -11,6 +12,6 @@ import frc.robot.drivetrain.DriveTrain;
 public class DriveAndCollectGSR1 extends ParallelCommandGroup {
 
     public DriveAndCollectGSR1(DriveTrain driveTrain, Collector collector) {
-        super(new OpenAndCollect(collector, () -> 0.5), new MoveByPath(driveTrain, GALACTIC_SEARCH_BLUE_FIRST));
+        super(new OpenAndCollect(collector, () -> 0.5), new MoveByPath(driveTrain, GALACTIC_SEARCH_RED_FIRST));
     }
 }
