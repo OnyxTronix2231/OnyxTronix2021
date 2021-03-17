a1004bd070908dfd5b061492646e49397c2fdadf
DanGoldenberg <75333169+DanGoldenberg@users.noreply.github.com>
2021-03-17 16:46:48 +0200

    fixed small bug

--- src/main/java/frc/robot/vision/visionGSC/VisionGSC.java
+++ src/main/java/frc/robot/vision/visionGSC/VisionGSC.java
@@ -10,7 +10,8 @@ public class VisionGSC extends BaseVision{
     private GSCOption chosenOption;
 
     public VisionGSC(){
-        Shuffleboard.getTab("Vision").addString("Chosen path", () -> chosenOption.toString());
+        Shuffleboard.getTab("Vision").addString("Chosen path", () -> chosenOption != null ? chosenOption.toString() : "no path");
+        Shuffleboard.getTab("Vision").addNumber("Area precentage", this::getTargetAreaPercentage);
     }
 
     public double getTargetAreaPercentage() {
