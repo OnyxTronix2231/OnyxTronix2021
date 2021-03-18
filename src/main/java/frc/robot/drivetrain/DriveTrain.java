6de6931bd2b5d3afc22a1abe0762f6441b41bf26
CluePen <clu.pen12@gmail.com>
2021-03-18 17:04:25 +0200

    Removed unnecessary commented and noncommented testing methods.

--- src/main/java/frc/robot/drivetrain/DriveTrain.java
+++ src/main/java/frc/robot/drivetrain/DriveTrain.java
@@ -21,7 +21,6 @@ import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.RobotController;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.kinematics.DifferentialDriveWheelSpeeds;
@@ -33,9 +32,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Robot;
 
-import java.io.PrintWriter;
-import java.util.function.Supplier;
-
 public class DriveTrain extends SubsystemBase {
 
     private final DriveTrainComponents components;
@@ -43,11 +39,6 @@ public class DriveTrain extends SubsystemBase {
     private final DriveTrainVirtualComponents virtualComponents;
     private final SendableChooser<Command> autoPathChooser;
     private final SendableChooser<Pose2d> startPoseChooser;
-    //  private final NetworkTableEntry voltageInputEntry;
-//  private final NetworkTableEntry resetEntry;
-    private final Timer kaTimer;
-    private double maxLeft;
-    private double maxRight;
 
     public DriveTrain(DriveTrainComponents components, SimulationDriveTrainComponents simulationComponents,
                       DriveTrainVirtualComponents virtualComponents) {
@@ -60,7 +51,6 @@ public class DriveTrain extends SubsystemBase {
         autoPathChooser.addOption("AutoNav Second", AUTONAV_SECOND.toCommand(this));
         autoPathChooser.addOption("AutoNav Third", AUTONAV_THIRD_A.toCommand(this, AUTONAV_THIRD_B,
                 AUTONAV_THIRD_C, AUTONAV_THIRD_D));
-
 
         autoPathChooser.addOption("Galactic Search Red First", GALACTIC_SEARCH_RED_FIRST.toCommand(this));
         autoPathChooser.addOption("Galactic Search Red Second", GALACTIC_SEARCH_RED_SECOND.toCommand(this));
@@ -77,10 +67,6 @@ public class DriveTrain extends SubsystemBase {
         startPoseChooser.addOption("Galactic Search BlueSecond", GALACTIC_SEARCH_BLUE_SECOND.getStartPose());
         Shuffleboard.getTab("Autonomous").add("Autonomous Path Chooser", autoPathChooser);
         Shuffleboard.getTab("Autonomous").add("Autonomous Start Pose Chooser", startPoseChooser);
-//    voltageInputEntry = Shuffleboard.getTab("DriveTrain").add("VoltageInput", 0).getEntry();
-        kaTimer = new Timer();
-//    resetEntry = Shuffleboard.getTab("DriveTrain").add("Reset", 0).getEntry();
-        initKaTimer();
         if (Robot.isSimulation()) {
             Shuffleboard.getTab("DriveTrain").add("Field", getField2d());
             Shuffleboard.getTab("DriveTrain").addNumber("actualVoltage",
@@ -89,66 +75,27 @@ public class DriveTrain extends SubsystemBase {
                     () -> encoderUnitsDeciSecToMetersSec(getSimLeftMaster().getSelectedSensorVelocity()));
             getField2d().setRobotPose(START_POSE);
             getDriveTrainSim().setPose(START_POSE);
-        } else {
-//      Shuffleboard.getTab("DriveTrain").addNumber("actualVoltage",
-//          () -> getLeftMaster().getMotorOutputVoltage());
-//      Shuffleboard.getTab("DriveTrain").addNumber("speed left",
-//          () -> encoderUnitsDeciSecToMetersSec(getLeftMaster().getSelectedSensorVelocity()));
-//      Shuffleboard.getTab("DriveTrain").addNumber("speed right",
-//          () -> encoderUnitsDeciSecToMetersSec(getRightMaster().getSelectedSensorVelocity()));
-//      Shuffleboard.getTab("DriveTrain").addNumber("max speed left",
-//          () -> maxLeft);
-//      Shuffleboard.getTab("DriveTrain").addNumber("max speed right",
-//          () -> maxRight);
-//      Shuffleboard.getTab("DriveTrain").addNumber("START X",
-//              () -> START_POSE.getX());
-//      Shuffleboard.getTab("DriveTrain").addNumber("START Y",
-//              () -> START_POSE.getY());
-//      Shuffleboard.getTab("DriveTrain").addNumber("START ROTATION DEGREES",
-//              () -> START_POSE.getRotation().getDegrees());
-            Shuffleboard.getTab("DriveTrain").addNumber("CURRENT X",
-                    () -> virtualComponents.getOdometry().getPoseMeters().getX());
-            Shuffleboard.getTab("DriveTrain").addNumber("CURRENT Y",
-                    () -> virtualComponents.getOdometry().getPoseMeters().getY());
-            Shuffleboard.getTab("DriveTrain").addNumber("CURRENT ROTATION DEGREES",
-                    () -> virtualComponents.getOdometry().getPoseMeters().getRotation().getDegrees());
-            components.getNormelizedPigeonIMU().setYaw(0);
         }
+        Shuffleboard.getTab("DriveTrain").addNumber("CURRENT X",
+                () -> virtualComponents.getOdometry().getPoseMeters().getX());
+        Shuffleboard.getTab("DriveTrain").addNumber("CURRENT Y",
+                () -> virtualComponents.getOdometry().getPoseMeters().getY());
+        Shuffleboard.getTab("DriveTrain").addNumber("CURRENT ROTATION DEGREES",
+                () -> virtualComponents.getOdometry().getPoseMeters().getRotation().getDegrees());
+        components.getNormelizedPigeonIMU().setYaw(0);
+
         virtualComponents.getOdometry().resetPosition(START_POSE, START_POSE.getRotation());
         resetEncoders();
     }
 
-//  public double getShuffleboardVoltage() {
-//    return voltageInputEntry.getDouble(0);
-//  }
-
     @Override
     public void periodic() {
-//    if (maxLeft < encoderUnitsDeciSecToMetersSec(getLeftMaster().getSelectedSensorVelocity()))
-//      maxLeft = encoderUnitsDeciSecToMetersSec(getLeftMaster().getSelectedSensorVelocity());
-//    if (maxRight < encoderUnitsDeciSecToMetersSec(getRightMaster().getSelectedSensorVelocity()))
-//      maxRight = encoderUnitsDeciSecToMetersSec(getRightMaster().getSelectedSensorVelocity());
-//        if (Robot.isReal()) {
-//        double speed = encoderUnitsDeciSecToMetersSec(getLeftMaster().getSelectedSensorVelocity());
-//        if (resetEntry.getDouble(0) == 1)
-//            maxSpeedAtVoltage = 0;
-//        else if (speed > maxSpeedAtVoltage)
-//            maxSpeedAtVoltage = speed;
-//        System.out.println(maxSpeedAtVoltage);
-//        System.out.println(getLeftMaster().getSupplyCurrent());
-//        }
-//    if (resetEntry.getDouble(0) == 1) {
-//      maxLeft = 0;
-//      maxRight = 0;
-//    }
         virtualComponents.getOdometry().update(
                 Rotation2d.fromDegrees(-getHeading()),
                 encoderUnitsToMeters(Robot.isSimulation() ? getSimLeftMaster().getSelectedSensorPosition() :
                         getLeftMaster().getSelectedSensorPosition()),
                 encoderUnitsToMeters(Robot.isSimulation() ? getSimRightMaster().getSelectedSensorPosition() :
                         getRightMaster().getSelectedSensorPosition()));
-
-//        System.out.println(encoderUnitsDeciSecToMetersSec(getLeftMaster().getSelectedSensorVelocity()));
     }
 
     @Override
@@ -343,32 +290,5 @@ public class DriveTrain extends SubsystemBase {
 
     public void resetOdometryToChosenPath() {
         resetOdometryToPose(startPoseChooser.getSelected());
-    }
-
-    private void initKaTimer() {
-        kaTimer.reset();
-        kaTimer.start();
-    }
-
-    public void kaPrints(PrintWriter left, PrintWriter right) {
-        double leftSpeed = Robot.isSimulation() ?
-                encoderUnitsDeciSecToMetersSec(getSimLeftMaster().getSelectedSensorVelocity()) : encoderUnitsDeciSecToMetersSec(getLeftMaster().getSelectedSensorVelocity());
-        double leftVoltage = Robot.isSimulation() ?
-                getSimLeftMaster().getMotorOutputVoltage() : getLeftMaster().getMotorOutputVoltage();
-
-        double rightSpeed = Robot.isSimulation() ?
-                encoderUnitsDeciSecToMetersSec(getSimRightMaster().getSelectedSensorVelocity()) : encoderUnitsDeciSecToMetersSec(getRightMaster().getSelectedSensorVelocity());
-        double rightVoltage = Robot.isSimulation() ?
-                getSimRightMaster().getMotorOutputVoltage() : getRightMaster().getMotorOutputVoltage();
-
-        try {
-            if (leftSpeed != 0 && leftVoltage != 0) {
-                left.println(kaTimer.get() + "," + leftSpeed + "," + leftVoltage);
-            }
-            if (rightSpeed != 0 && rightVoltage != 0)
-                right.println(kaTimer.get() + "," + rightSpeed + "," + rightVoltage);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
