cbeaab803c03054742891683dfb97ff16c7f5984
YuvalRashman <56125252+YuvalRashman@users.noreply.github.com>
2021-05-10 21:34:30 +0300

    did some changes to ShootBall

--- src/main/java/frc/robot/crossPlatform/shooter/ShootBall.java
+++ src/main/java/frc/robot/crossPlatform/shooter/ShootBall.java
@@ -1,21 +1,23 @@
 package frc.robot.crossPlatform.shooter;
 
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.arc.Arc;
 import frc.robot.arc.commands.MoveArcToDistance;
 import frc.robot.ballTrigger.BallTrigger;
-import frc.robot.ballTrigger.commands.OpenBallTriggerPiston;
+import frc.robot.crossPlatform.conveyor.SpinRevolverAndTriggerThenOpenTriggerPiston;
+import frc.robot.revolver.Revolver;
 import frc.robot.shooter.Shooter;
 import frc.robot.shooter.commands.SpinShooterByDistance;
-import frc.robot.shooter.commands.WaitUntilShooterOnTarget;
 import frc.robot.vision.visionMainChallenge.Vision;
 
-public class ShootBall extends SequentialCommandGroup {
+public class ShootBall extends ParallelCommandGroup {
 
-    public ShootBall(Shooter shooter, Vision vision, BallTrigger ballTrigger, Arc arc) {
-        super(new MoveArcToDistance(arc, () -> vision.getChosenTarget().getAirDistanceTurretToTarget()),
-                new SpinShooterByDistance(shooter, () -> vision.getChosenTarget().getAirDistanceTurretToTarget()),
-                new WaitUntilShooterOnTarget(shooter),
-                new OpenBallTriggerPiston(ballTrigger));
+    public ShootBall(Shooter shooter, Vision vision, BallTrigger ballTrigger, Arc arc, Revolver revolver) {
+        super(new SpinShooterByDistance(shooter, () -> vision.getChosenTarget().getAirDistanceTurretToTarget()),
+                new MoveArcToDistance(arc, () -> vision.getChosenTarget().getAirDistanceTurretToTarget()),
+                (new WaitUntilCommand(shooter::isOnTarget).alongWith(
+                        new WaitUntilCommand(arc::isOnTarget))).andThen(
+                        new SpinRevolverAndTriggerThenOpenTriggerPiston(revolver, ballTrigger, () -> 1, () -> 1)));
     }
 }
