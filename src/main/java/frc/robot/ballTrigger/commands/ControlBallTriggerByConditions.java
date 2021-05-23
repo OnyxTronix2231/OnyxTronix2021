a616629182194222a332822e4a78929ec43d6ee0
yuval <yrashman@gmail.com>
2021-05-23 21:54:33 +0300

    fix to last commit

--- src/main/java/frc/robot/ballTrigger/commands/ControlBallTriggerByConditions.java
+++ src/main/java/frc/robot/ballTrigger/commands/ControlBallTriggerByConditions.java
@@ -4,9 +4,6 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.ballTrigger.BallTrigger;
-import frc.robot.ballTrigger.commands.CloseBallTriggerPiston;
-import frc.robot.ballTrigger.commands.OpenBallTriggerPiston;
-import frc.robot.ballTrigger.commands.SpinBallTriggerBySpeed;
 
 import java.util.Arrays;
 import java.util.function.BooleanSupplier;
@@ -14,31 +11,31 @@ import java.util.function.DoubleSupplier;
 
 public class ControlBallTriggerByConditions extends SequentialCommandGroup {
 
-  private BallTrigger ballTrigger;
+    private BallTrigger ballTrigger;
 
-  public ControlBallTriggerByConditions(BallTrigger ballTrigger, DoubleSupplier ballTriggerSpeedSupplier,
-                                        BooleanSupplier... isReadyConditions) {
-    super(
-        new WaitUntilCommand(() -> Arrays.stream(isReadyConditions).allMatch(BooleanSupplier::getAsBoolean)),
-        new OpenBallTriggerPiston(ballTrigger),
-        new SpinBallTriggerBySpeed(ballTrigger, ballTriggerSpeedSupplier),
-        new WaitUntilCommand(() -> Arrays.stream(isReadyConditions).
-            anyMatch(isReadyCondition -> !isReadyCondition.getAsBoolean())),
-        new CloseBallTriggerPiston(ballTrigger));
-    this.ballTrigger = ballTrigger;
-  }
-
-  @Override
-  public boolean isFinished() {
-    if (super.isFinished()) {
-      initialize();
+    public ControlBallTriggerByConditions(BallTrigger ballTrigger, DoubleSupplier ballTriggerSpeedSupplier,
+                                          BooleanSupplier... isReadyConditions) {
+        super(
+                new WaitUntilCommand(() -> Arrays.stream(isReadyConditions).allMatch(BooleanSupplier::getAsBoolean)),
+                new OpenBallTriggerPiston(ballTrigger),
+                new SpinBallTriggerBySpeed(ballTrigger, ballTriggerSpeedSupplier),
+                new WaitUntilCommand(() -> Arrays.stream(isReadyConditions).
+                        anyMatch(isReadyCondition -> !isReadyCondition.getAsBoolean())),
+                new CloseBallTriggerPiston(ballTrigger));
+        this.ballTrigger = ballTrigger;
     }
-    return false;
-  }
 
-  @Override
-  public void end(boolean interrupted) {
-    super.end(interrupted);
-    CommandScheduler.getInstance().schedule(new CloseBallTriggerPiston(ballTrigger));
-  }
+    @Override
+    public boolean isFinished() {
+        if (super.isFinished()) {
+            initialize();
+        }
+        return false;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        super.end(interrupted);
+        CommandScheduler.getInstance().schedule(new CloseBallTriggerPiston(ballTrigger));
+    }
 }
