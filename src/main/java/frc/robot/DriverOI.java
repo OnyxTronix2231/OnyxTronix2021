522f34e6cb8a49f7d362f509af9b0df372c2fb70
yuval <yrashman@gmail.com>
2021-06-04 14:14:33 +0300

    change PIDF values to correct one and also changed MaxVelocity Value

--- src/main/java/frc/robot/DriverOI.java
+++ src/main/java/frc/robot/DriverOI.java
@@ -1,21 +1,8 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.arc.Arc;
-import frc.robot.ballTrigger.BallTrigger;
-import frc.robot.ballTrigger.BallTriggerComponents;
-import frc.robot.collector.Collector;
-import frc.robot.crossPlatform.DriverCrossPlatformOIBinder;
-import frc.robot.drivetrain.DriveTrain;
-import frc.robot.drivetrain.DriveTrainOiBinder;
-import frc.robot.revolver.Revolver;
+import frc.robot.shooter.DriverShooterOiBinder;
 import frc.robot.shooter.Shooter;
-import frc.robot.shooter.ShooterOi;
-import frc.robot.turret.Turret;
-import frc.robot.vision.visionMainChallenge.Vision;
 import onyxTronix.JoystickAxis;
 
 import static frc.robot.RobotConstants.DRIVER_JOYSTICK_PORT;
@@ -27,7 +14,7 @@ public class DriverOI {
     public DriverOI(Shooter shooter) {
         xboxController = new XboxController(DRIVER_JOYSTICK_PORT);
         JoystickAxis shootTrigger = new JoystickAxis(xboxController, XboxController.Axis.kLeftY.value);
-        new ShooterOi(shooter, shootTrigger);
+        new DriverShooterOiBinder(shooter, shootTrigger);
     }
 }
 
--- src/main/java/frc/robot/shooter/DriverShooterOiBinder.java
+++ src/main/java/frc/robot/shooter/DriverShooterOiBinder.java
@@ -1,11 +1,12 @@
 package frc.robot.shooter;
 
+import frc.robot.shooter.commands.SpinShooterByRPM;
 import frc.robot.shooter.commands.SpinShooterBySpeed;
 import onyxTronix.JoystickAxis;
 
 public class DriverShooterOiBinder {
 
     public DriverShooterOiBinder(Shooter shooter, JoystickAxis shootBySpeed) {
-        shootBySpeed.whileActiveContinuous(new SpinShooterBySpeed(shooter, shootBySpeed::getRawAxis));
+            shootBySpeed.whileActiveContinuous(new SpinShooterByRPM(shooter, ()-> 6000));
     }
 }
