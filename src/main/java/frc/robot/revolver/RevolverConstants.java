902977ab46b7245e682b816fce7791b94d3133bd
NadaVibe <nadav8775@gmail.com>
2021-06-08 21:58:04 +0300

    Fixed Revolver by testing

--- src/main/java/frc/robot/revolver/RevolverConstants.java
+++ src/main/java/frc/robot/revolver/RevolverConstants.java
@@ -4,7 +4,7 @@ public final class RevolverConstants {
 
     static final int DECISECOND_IN_MIN = 600;
     static final int TOLERANCE_IN_RPM = 3; //TODO: check value
-    static final double CONVERSION_RATE = 26.5625; //1:26.5625
+    static final double CONVERSION_RATE = 26.5625; //26.5625:1
     static final double ENCODER_UNITS_PER_ROTATION = 2048 * CONVERSION_RATE; //TODO: check value
 
     public static final class RevolverComponentsA {
@@ -17,10 +17,10 @@ public final class RevolverConstants {
         static final int STATOR_TRIGGER_THRESHOLD_TIME = 0; //TODO: check value
         static final int STATOR_TRIGGER_THRESHOLD_CURRENT = 0; //TODO: check value
         static final double MAX_CLOSED_LOOP_OUTPUT = 1023;
-        static final double MAX_VELOCITY = 0; //TODO: check value
-        static final double VELOCITY_P = 0; //TODO: check value
+        static final double MAX_VELOCITY = 21900; //TODO: check value
+        static final double VELOCITY_P = 0.05; //TODO: check value
         static final double VELOCITY_I = 0; //TODO: check value
-        static final double VELOCITY_D = 0; //TODO: check value
+        static final double VELOCITY_D = 2; //TODO: check value
         static final double VELOCITY_F = MAX_CLOSED_LOOP_OUTPUT / MAX_VELOCITY;
         static final double OPEN_LOOP_RAMP = 0; //TODO: check value
         static final double CLOSED_LOOP_RAMP = 0; //TODO: check value
