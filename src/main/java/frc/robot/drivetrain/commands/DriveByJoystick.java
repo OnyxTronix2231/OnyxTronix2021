17d0a6853e7345b1d1f8761634db7bf8f91f8198
AE173 <56049042+AE173@users.noreply.github.com>
2021-06-09 00:32:28 +0300

    Fixed drivetrain and calibrating

--- src/main/java/frc/robot/drivetrain/commands/DriveByJoystick.java
+++ src/main/java/frc/robot/drivetrain/commands/DriveByJoystick.java
@@ -6,7 +6,7 @@ import frc.robot.drivetrain.DriveTrain;
 
 public class DriveByJoystick extends DriveBySpeed {
     public DriveByJoystick(DriveTrain driveTrain, XboxController driveJoystick) {
-        super(driveTrain, () -> driveJoystick.getX(GenericHID.Hand.kRight),
-                () -> -driveJoystick.getY(GenericHID.Hand.kLeft));
+        super(driveTrain, () -> -driveJoystick.getRawAxis(XboxController.Axis.kLeftY.value),
+                () -> driveJoystick.getRawAxis(XboxController.Axis.kRightX.value));
     }
 }
