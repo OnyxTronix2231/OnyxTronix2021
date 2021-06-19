608391e03aaff0920f424cdd0aeaaf8ab2a18287
NadaVibe <nadav8775@gmail.com>
2021-06-19 21:41:42 +0300

    Fixed Ball trigger by testing

--- src/main/java/frc/robot/ballTrigger/BallTriggerComponents.java
+++ src/main/java/frc/robot/ballTrigger/BallTriggerComponents.java
@@ -11,8 +11,6 @@ public interface BallTriggerComponents {
 
     WPI_TalonSRX getMasterMotor();
 
-    IMotorController getSlaveMotor();
-
     DoubleSolenoid getSolenoid();
 
     Counter getEncoder();
--- src/main/java/frc/robot/ballTrigger/BallTriggerConstants.java
+++ src/main/java/frc/robot/ballTrigger/BallTriggerConstants.java
@@ -4,8 +4,9 @@ import edu.wpi.first.wpilibj.DoubleSolenoid;
 
 public final class BallTriggerConstants {
 
-    static final int ENCODER_UNITS_PER_ROTATION = 4096;
     static final int DECISECOND_IN_MIN = 600;
+    static final double CONVERSION_RATE = 6.25;
+    static final double ENCODER_UNITS_PER_ROTATION = 4096 * CONVERSION_RATE;
     static final DoubleSolenoid.Value OPEN_PISTON = DoubleSolenoid.Value.kForward;
     static final DoubleSolenoid.Value CLOSE_PISTON = DoubleSolenoid.Value.kReverse;
 
@@ -20,12 +21,16 @@ public final class BallTriggerConstants {
         static final int CONTINUOUS_CURRENT_LIMIT = 0; //TODO: check value
         static final int OPEN_LOOP_RAMP = 0; //TODO: check value
         static final int CLOSED_LOOP_RAMP = 0; //TODO: check value
+        static final int TOLERANCE_IN_RPM = 20;
+        static final int PEAK_OUTPUT_FORWARD = 1;
         static final double MAX_CLOSED_LOOP_OUTPUT = 1023;
-        static final double MAX_VELOCITY = 0; //TODO: check value
-        static final double VELOCITY_P = 0; //TODO: check value
+        static final double MAX_VELOCITY = 18500; //TODO: check value
+        static final double VELOCITY_P = 0.02; //TODO: check value
         static final double VELOCITY_I = 0; //TODO: check value
-        static final double VELOCITY_D = 0; //TODO: check value
+        static final double VELOCITY_D = 1; //TODO: check value
         static final double VELOCITY_F = MAX_CLOSED_LOOP_OUTPUT / MAX_VELOCITY;
         static final boolean CURRENT_LIMIT_ENABLED = false;
+        static final boolean INVERTED = false;
+        static final boolean SENSOR_PHASE = true;
     }
 }
