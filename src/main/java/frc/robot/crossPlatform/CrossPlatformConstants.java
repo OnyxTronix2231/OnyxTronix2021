abfc1805365f75d385996cda72f397ecb330d0c2
AE173 <56049042+AE173@users.noreply.github.com>
2021-07-03 16:03:18 +0300

    Conveyot constants ae now public in crossPlatform

--- src/main/java/frc/robot/crossPlatform/CrossPlatformConstants.java
+++ src/main/java/frc/robot/crossPlatform/CrossPlatformConstants.java
@@ -4,14 +4,14 @@ public class CrossPlatformConstants {
 
     public static class ConveyorConstantsA {
 
-        static final double REVOLVER_SPEED_WHILE_COLLECTING = 0.15;
-        static final double REVOLVER_SPEED_WHILE_SHOOTING = 0.2;
-        static final double REVOLVER_RPM_WHILE_SHOOTING = 50;
-        static final double REVOLVER_RPM_WHILE_COLLECTING = 25;
+        public static final double REVOLVER_SPEED_WHILE_COLLECTING = 0.15;
+        public static final double REVOLVER_SPEED_WHILE_SHOOTING = 0.2;
+        public static final double REVOLVER_RPM_WHILE_SHOOTING = 50;
+        public static final double REVOLVER_RPM_WHILE_COLLECTING = 25;
     }
     public static class CollectorConstantsA {
 
-        static final double TESTING_SPEED_COLLECTOR = 0.7;
+        public static final double TESTING_SPEED_COLLECTOR = 0.5;
     }
 
     public static class TriggerConstantsA {
