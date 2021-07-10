a74a686f9726a2e7ec5d351ea86836104f1a6032
YuvalRashman <56125252+YuvalRashman@users.noreply.github.com>
2021-07-10 15:38:53 +0300

    fix

--- src/main/java/frc/robot/crossPlatform/ShootBall.java
+++ src/main/java/frc/robot/crossPlatform/ShootBall.java
@@ -6,13 +6,11 @@ import static frc.robot.crossPlatform.CrossPlatformConstants.TriggerConstantsA.B
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.arc.Arc;
-import frc.robot.arc.commands.CalibrateArc;
 import frc.robot.arc.commands.CloseArc;
 import frc.robot.arc.commands.MoveArcByVision;
 import frc.robot.ballTrigger.BallTrigger;
 import frc.robot.ballTrigger.commands.ControlBallTriggerByConditions;
 import frc.robot.ballTrigger.commands.SpinBallTriggerByRPM;
-import frc.robot.ballTrigger.commands.SpinBallTriggerBySpeed;
 import frc.robot.revolver.Revolver;
 import frc.robot.revolver.commands.SpinRevolverByRPM;
 import frc.robot.shooter.Shooter;
@@ -27,7 +25,7 @@ public class ShootBall extends ParallelCommandGroup {
     public ShootBall(Shooter shooter, BallTrigger ballTrigger, Arc arc,
                      YawControl yawControl, Vision vision, Revolver revolver, Trigger shootBall) {
         super(
-                new SpinBallTriggerBySpeed(ballTrigger, () -> 0.8),
+                new SpinBallTriggerByRPM(ballTrigger, () -> BALL_TRIGGER_RPM),
                 new SpinRevolverByRPM(revolver, () -> REVOLVER_RPM_WHILE_SHOOTING),
                 new SmartMoveTurretToTargetArea(yawControl, vision).andThen(
                         new MoveTurretByVision(yawControl, vision).alongWith(
