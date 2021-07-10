ed30b2dab1e40aadf70d4c0f3c931580608ab6c0
YuvalRashman <56125252+YuvalRashman@users.noreply.github.com>
2021-07-10 21:54:36 +0300

    fix

--- src/main/java/frc/robot/crossPlatform/CrossPlatformConstants.java
+++ src/main/java/frc/robot/crossPlatform/CrossPlatformConstants.java
@@ -10,14 +10,17 @@ public class CrossPlatformConstants {
         static final double REVOLVER_RPM_WHILE_COLLECTING = 25;
     }
     public static class CollectorConstantsA {
+
         static final double TESTING_SPEED_COLLECTOR = 0.7;
     }
 
     public static class TriggerConstantsA {
+
         public static final double BALL_TRIGGER_RPM = 3000;
     }
 
     public static class ShooterConstantA{
+
         public static final double CLOSE_SHOOTER_RPM = 3000;
         public static final double AUTONOMOUS_BALL_SHOOT_TIME = 4;
     }
