cb25f443eba73ae81d37d97e1e4f6df33b8d0a6c
AE173 <56049042+AE173@users.noreply.github.com>
2021-07-11 12:28:43 +0300

    made crossplatform constants public

--- src/main/java/frc/robot/crossPlatform/CrossPlatformConstants.java
+++ src/main/java/frc/robot/crossPlatform/CrossPlatformConstants.java
@@ -4,12 +4,12 @@ public class CrossPlatformConstants {
 
     public static class ConveyorConstantsA {
 
-        static final double REVOLVER_RPM_WHILE_SHOOTING = 60;
-        static final double REVOLVER_RPM_WHILE_COLLECTING = 25;
+        public static final double REVOLVER_RPM_WHILE_SHOOTING = 60;
+        public static final double REVOLVER_RPM_WHILE_COLLECTING = 25;
     }
     public static class CollectorConstantsA {
 
-        static final double TESTING_SPEED_COLLECTOR = 0.8;
+        public static final double TESTING_SPEED_COLLECTOR = 0.8;
     }
 
     public static class TriggerConstantsA {
