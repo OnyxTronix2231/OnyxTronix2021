f557319c1b11fafc3b9596a601ebd7e4f9626831
nimrod46 <nimrodma99@gmail.com>
2022-01-08 18:37:47 +0200

    Update OpenPiston.java

--- src/main/java/frc/robot/Roulette/commands/OpenPiston.java
+++ src/main/java/frc/robot/Roulette/commands/OpenPiston.java
@@ -2,7 +2,7 @@ package frc.robot.Roulette.commands;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.Roulette.Roulette;
-//why does this class exist if never used?
+
 public class OpenPiston extends InstantCommand {
 
     public OpenPiston(Roulette roulette) {
