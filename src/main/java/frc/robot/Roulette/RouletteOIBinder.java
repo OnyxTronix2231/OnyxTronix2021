b4f796aadee9673fc9236a7803c2fb385e7ae268
nimrod46 <nimrodma99@gmail.com>
2022-01-08 18:37:06 +0200

    Update RouletteOIBinder.java

--- src/main/java/frc/robot/Roulette/RouletteOIBinder.java
+++ src/main/java/frc/robot/Roulette/RouletteOIBinder.java
@@ -2,7 +2,7 @@ package frc.robot.Roulette;
 
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Roulette.commands.SpinToColor;
-//why does this class exist if never used?
+
 public class RouletteOIBinder {
 
     public RouletteOIBinder(Roulette roulette, Trigger spinToGameColor) {
