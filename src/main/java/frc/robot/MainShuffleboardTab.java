c99b420b007edfd86e77738a8d2a0e4ea3827720
YuvalGur135 <75333129+YuvalGur135@users.noreply.github.com>
2022-02-05 21:55:56 +0200

    shoot while driving updtae

--- src/main/java/frc/robot/MainShuffleboardTab.java
+++ src/main/java/frc/robot/MainShuffleboardTab.java
@@ -7,22 +7,15 @@ import frc.robot.arc.Arc;
 import frc.robot.ballTrigger.BallTrigger;
 import frc.robot.revolver.Revolver;
 import frc.robot.shooter.Shooter;
-import frc.robot.vision.visionMainChallenge.InnerTarget;
-import frc.robot.vision.visionMainChallenge.Vision;
 import frc.robot.yawControll.YawControl;
 
 import java.util.Map;
 
 public class MainShuffleboardTab {
 
-    public MainShuffleboardTab(Shooter shooter, Revolver revolver, BallTrigger ballTrigger, Arc arc, Vision vision,
-                               YawControl yawControl, HttpCamera limeLightFeed, UsbCamera camera1, UsbCamera camera2){
+    public MainShuffleboardTab(Shooter shooter, Revolver revolver, BallTrigger ballTrigger, Arc arc,
+                               YawControl yawControl, HttpCamera limeLightFeed){
 
-        Shuffleboard.getTab("Main").addString("Vision target",
-                ()-> vision.getChosenTarget() instanceof InnerTarget ? "Inner Target" : "Outer target")
-                .withPosition(0,0);
-        Shuffleboard.getTab("Main").addNumber("Distance to outer target",
-                vision.getOuterTarget()::getAirDistanceTurretToTarget).withPosition(1,0);
         Shuffleboard.getTab("Main").addBoolean("Arc on Target",
                 arc::isOnTarget).withPosition(2,0);
         Shuffleboard.getTab("Main").addBoolean("Ball Trigger on Target",
@@ -33,10 +26,6 @@ public class MainShuffleboardTab {
                 yawControl::isOnTarget).withPosition(5,0);
         Shuffleboard.getTab("Main").addBoolean("Shooter on Target",
                 shooter::isOnTarget).withPosition(6,0);
-        Shuffleboard.getTab("Main").add(camera1).withPosition(0,1).withSize(3,3)
-                .withProperties(Map.of("Show Crosshair", false));
-        Shuffleboard.getTab("Main").add(camera2).withPosition(3, 1).withSize(3,3)
-                .withProperties(Map.of("Show Crosshair", false));
         Shuffleboard.getTab("Main").add("LL", limeLightFeed).withPosition(6,1).withSize(3,3)
                 .withProperties(Map.of("Show Crosshair", false));
     }
